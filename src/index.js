f51cfa9608aeda841d6ffaf0fd7432e8a93a8208
Anna Sheaffer <anna2@users.noreply.github.com>
2021-08-11 15:04:49 -0500

    lift state to game component; start/stop generations counter

--- src/index.js
+++ src/index.js
@@ -10,26 +10,18 @@ function Cell(props) {
 };
 
 class Board extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      cells: Array(10)
-              .fill()
-              .map(e => Array(10).fill().map((e, i) => false))
-    }
-  }
 
   renderCell(x, y) {
     return (
       <Cell
-        value={this.state.cells[x][y]}
+        value={this.props.cells[x][y]}
       />
     );
   }
 
   render() {
     return (
-      this.state.cells.map((row, rowIndex) =>
+      this.props.cells.map((row, rowIndex) =>
         <div className="board-row">
           {row.map((cell, cellIndex) => this.renderCell(rowIndex, cellIndex))}
         </div>
@@ -38,4 +30,53 @@ class Board extends React.Component {
   }
 }
 
-ReactDOM.render(<Board />, document.getElementById("root"));
+class Game extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cells: Array(10)
+              .fill()
+              .map(e => Array(10).fill().map((e, i) => false)),
+      paused: true,
+      generations: 0,
+      intervalRef: null
+    }
+  }
+
+  toggleStartStop() {
+    this.setState({
+      paused: !this.state.paused
+    },
+    () => {
+      if (this.state.paused) {
+        clearInterval(this.state.intervalRef)
+      } else {
+        this.setState({intervalRef: setInterval(this.tick, 1000)})
+      }
+    })
+  }
+
+  tick = () => {
+    this.setState({
+      generations: this.state.generations + 1
+    })
+  }
+
+  render() {
+    return (
+      <div className="game">
+        <div className="game__board">
+          <Board
+            cells={this.state.cells}
+          />
+        </div>
+        <div className="game__info">
+          <button onClick={() => this.toggleStartStop()}>{this.state.paused ? "Start" : "Stop"}</button>
+          <div>{this.state.generations}</div>
+        </div>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<Game />, document.getElementById("root"));
